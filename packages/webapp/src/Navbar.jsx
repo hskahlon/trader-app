336c1d6291bdb4fecda333edd69ab9e1239b7efa
wca93 <steven_cao_2@sfu.ca>
2021-07-10 12:34:05 -0700

    basic navbar with different pages

--- packages/webapp/src/Navbar.jsx
+++ packages/webapp/src/Navbar.jsx
@@ -1,45 +1,36 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { Tabs, Tab, AppBar } from "@material-ui/core";
+import About from "./About";
+import Home from "./Home";
+import Stocks from "./Stocks";
+import Trade from "./Trade";
+import Profile from "./Profile";
 
 function Navbar() {
-  const classes = useStyles();
+  const [tab, setTab] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setTab(newValue);
+  };
+
   return (
-    <div className={classes.root}>
+    <>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+        <Tabs value={tab} onChange={handleChange}>
+          <Tab label="Home" />
+          <Tab label="Stocks" />
+          <Tab label="Trade" />
+          <Tab label="Profile" />
+          <Tab label="About" />
+        </Tabs>
       </AppBar>
-    </div>
+
+      {tab === 0 && <Home />}
+      {tab === 1 && <Stocks />}
+      {tab === 2 && <Trade />}
+      {tab === 3 && <Profile />}
+      {tab === 4 && <About />}
+    </>
   );
 }
 
