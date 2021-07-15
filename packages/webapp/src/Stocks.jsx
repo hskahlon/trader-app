587a8858d7f4b7458165f15cd33ed8a68d2373f2
hskahlon <harjot_kahlon_2@sfu.ca>
2021-07-15 15:31:44 -0700

    Created intial chart with pre-filled data

--- packages/webapp/src/Stocks.jsx
+++ packages/webapp/src/Stocks.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Example from "./portfolio_chart";
 
 function Stocks() {
-  return <h1>this is Stocks page</h1>;
+  return (
+    <div className="App">
+      <h1>This is the Stocks pages</h1>
+      <h2>Currently the chart is working with supplied data</h2>
+      <Example></Example>
+    </div>
+  );
 }
 
 export default Stocks;
