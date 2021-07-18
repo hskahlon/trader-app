ab5ec5d39f54b7203a059feeaea8c3524c764fc1
hskahlon <harjot_kahlon_2@sfu.ca>
2021-07-18 16:37:15 -0700

    ARefactored Chart, improved speed and state management

--- packages/webapp/src/Stocks.jsx
+++ packages/webapp/src/Stocks.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Example from "./portfolio_chart";
-
+import SelectStock from "./components/selectStock";
+import ChartTest from "./components/chart";
 function Stocks() {
   return (
     <div className="App">
       <h1>This is the Stocks pages</h1>
-      <h2>Currently the chart is working with supplied data</h2>
-      <Example></Example>
+      <h2 id="Stock">TSLA</h2>
+      <SelectStock></SelectStock>
+      <ChartTest />
+      {/* <Example></Example> */}
     </div>
   );
 }
