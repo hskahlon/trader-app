95251a12219f8eae8960c1863a7289e5c6b65836
jfitzmor <joshua_fitz-morris@sfu.ca>
2021-07-18 17:30:02 -0700

    pls

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,28 +1,28 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import path from "path";
 
-import db from './db/index.js'
-import stockRoutes from './routes/stock.js'
-import userRoutes from './routes/users.js'
+import db from "./db/index.js";
+import stockRoutes from "./routes/stock.js";
+import userRoutes from "./routes/users.js";
 
-const app = express()
-const apiPort = 5000
+const app = express();
+const apiPort = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.get('/', (req, res) => {
-  res.send('Hello Worlddddd!')
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "hello from server" });
+});
 
-app.get('/test', (req, res) => {
-  res.send('testing')
-})
+app.use("/stock", stockRoutes);
+app.use("/user", userRoutes);
 
-app.use('/stock', stockRoutes)
-app.use('/user', userRoutes)
-
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../webapp/build", "index.html"));
+});
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
